5883b04b5691c2e49d52c07236b7fb365afb7e46
Arthus Ni <arthus8ni@gmail.com>
2021-09-01 20:55:07 +0200

    schedule service

--- src/main/java/com/udacity/jdnd/course3/critter/schedule/ScheduleService.java
+++ src/main/java/com/udacity/jdnd/course3/critter/schedule/ScheduleService.java
@@ -8,10 +8,12 @@ import com.udacity.jdnd.course3.critter.user.EmployeeRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-//import javax.transaction.Transactional;
+
+import javax.transaction.Transactional;
 
 
 @Service
+@Transactional
 public class ScheduleService {
 
     @Autowired
